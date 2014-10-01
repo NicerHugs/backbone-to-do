b3704212bb2c2a26fc459ac7a570e75736984cda
Jess Scheuring <jscheuring@gmail.com>
2014-10-01 18:05:21 -0400

    saving to see other one

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,21 +18,36 @@ var EditItemView = Backbone.View.extend({
     events: {
         'submit' : 'submit'
     },
-    submit: function() {
-
+    submit: function(e) {
+        e.preventDefault();
+        var date = this.$el.find(':nth-child(3) input').val() || new Date();
+        var data = {
+            title: this.$el.find(':first-child input').val() || '',
+            description: this.$el.find(':nth-child(2) input').val() || '',
+            date: new Date(date),
+            id: Date.now()
+        };
+        var newItem = new ToDoModel(data);
+        this.remove();
     }
 });
 
 var ItemView = Backbone.View.extend({
     tagName: 'li',
     className: 'todo-item',
-    template: _.template($('#todo-item-template').text())
+    template: _.template($('#todo-item-template').text()),
+    render: function(){
+        $('.todo-list').append(this.template(this.model));
+    }
 });
 
 var ListView = Backbone.View.extend({
     tagName: 'ul',
     className: 'todo-list',
-    template: _.template($('#todo-item-template').text())
+    initialize: function() {
+        $('.container').append(this.el);
+        this.listenTo(this.collection, 'add', console.log("hi"));
+      }
 });
 
 //==============================================================================
